8015bea8bfe38b176f9bce8526f39bb4e4d4fdd1
Zikai Liu <zikai.liu@outlook.com>
2024-02-04 14:15:22 +0100

    Fix pyinstaller bundled binary issue
    pyinstaller (new version?) bundles lame in a different directory.

--- bundle_files.py
+++ bundle_files.py
@@ -5,7 +5,8 @@ import sys
 # Note: when using these path, remember they may contain spaces in path
 if getattr(sys, 'frozen', False):
     application_path = os.path.dirname(sys.executable)
-    lame_filename = os.path.join(application_path, "lame")
+    lame_filename = os.path.join(application_path, "..", "Resources", "lame")
+    assert os.path.exists(lame_filename), f"lame not found: {lame_filename}"
 elif __file__:
     application_path = os.path.dirname(__file__)
     lame_filename = "lame"
